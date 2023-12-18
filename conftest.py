ceb58170028969ca533a6b86ccbad2934f9afd9c
ramnes <contact@ramnes.eu>
2023-12-18 10:03:23 +0100

    Try to upgrade to pytest-asyncio 0.23.2

--- conftest.py
+++ conftest.py
@@ -10,14 +10,6 @@ class Item(AsyncThingy):
     pass
 
 
-# this is required for async session scoped fixtures
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
 @pytest.fixture(autouse=True, scope="session")
 async def database():
     connect(
